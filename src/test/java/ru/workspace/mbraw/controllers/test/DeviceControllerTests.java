b984a26af7ffef6f838ce54746f855252e8a89e1
Voronin Dmitry <voronindev@gmail.com>
2015-04-23 14:54:58 +0300

    Добавлены дополнительные зависимости для теста аутентификации Добавлены тесты аутентификации:  - formAuthenticationSuccess  - formAuthenticationFailed  - httpBasicAuthenticationSuccess  - httpBasicAuthenticationFailed

--- src/test/java/ru/workspace/mbraw/controllers/test/DeviceControllerTests.java
+++ src/test/java/ru/workspace/mbraw/controllers/test/DeviceControllerTests.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -33,6 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ApplicationTestConfiguration.class)
 @WebAppConfiguration
+@ActiveProfiles("test")
 public class DeviceControllerTests extends Assert {
 
     @Autowired
